a0e4e1a4bee98982cbddc7678d20d93aabe80bb5
Fabrício Rissetto <fabriciorissetto@gmail.com>
2016-06-13 00:18:42 -0300

    Improving responsivity, sidebar toggle icon, and removing featured image from theme-setup page

--- assets/js/plugins/responsive-nav.js
+++ assets/js/plugins/responsive-nav.js
@@ -128,7 +128,7 @@ var responsiveNav = (function (window, document) {
       this.options = {
         animate: true,        // Boolean: Use CSS3 transitions, true or false
         transition: 400,      // Integer: Speed of the transition, in milliseconds
-        label: "Menu",        // String: Label for the navigation toggle
+        label: "",        // String: Label for the navigation toggle
         insert: "after",      // String: Insert the toggle before or after the navigation
         customToggle: "",     // Selector: Specify the ID of a custom toggle
         openPos: "relative",  // String: Position of the opened nav, relative or static
